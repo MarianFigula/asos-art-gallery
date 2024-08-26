a1cd5cade607058ec717b305b68d66cdc9fad338
Marian Figula <majofigulam3@gmail.com>
2024-08-26 19:02:33 +0200

    feat: api for creating user

---
+++ php/api/user/create.php
@@ -0,0 +1,35 @@
+<?php
+// api/user/create.php
+
+header("Content-Type: application/json");
+
+include_once '../../config/Database.php';
+include_once '../../classes/User.php';
+
+$database = new Database();
+$db = $database->getConnection();
+
+$user = new User($db);
+
+$data = json_decode(file_get_contents("php://input"));
+
+if (empty($data->email) || empty($data->password) ||
+    empty($data->security_question) || empty($data->security_answer)) {
+    echo json_encode(["status" => http_response_code(400) ,
+        "message" => "Incomplete data"]);
+    exit();
+}
+
+$user->setEmail($data->email);
+$user->setPassword(password_hash($data->password, PASSWORD_BCRYPT));
+$user->setSecurityQuestion($data->security_question);
+$user->setSecurityAnswer($data->security_answer);
+
+if (!$user->createUser()) {
+    echo json_encode(["status" => http_response_code(),
+        "message" => "Unable to create User"]);
+    exit();
+}
+
+echo json_encode(["status" => http_response_code(201),
+    "message" => "User created successfully"]);
