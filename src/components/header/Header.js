f3aaff856ce1e5b45a18944c660d835a46a2bcf8
Marian Figula <majofigulam3@gmail.com>
2024-08-27 16:53:39 +0200

    feat: header css

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,14 +2,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import "./Header.css"
+import userIcon from "../../assets/icons/person.svg"
+import listIcon from "../../assets/icons/list.svg"
 
 export function Header () {
     return (
         <header>
             <nav>
                 <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/login">Login</Link></li>
+                    <li><Link to="/"><h1>FEI Art Gallery</h1></Link></li>
+                    <li><Link to="/login"><img src={userIcon} alt="User Icon"/></Link></li>
+                    <li><Link to={""}><img src={listIcon} alt="Menu Icon"/></Link></li>
                     <li><Link to="/register">Register</Link></li>
                     {/* Add more links as needed */}
                 </ul>
