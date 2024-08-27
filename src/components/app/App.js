5d65e9e8b4540a4b414a3d49c391cdd927f9e0ce
Marian Figula <majofigulam3@gmail.com>
2024-08-27 11:40:31 +0200

    feat: added Header to App.js component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,16 +3,18 @@ import {Route, Routes} from "react-router-dom";
 import {Login} from "../login/Login";
 import {MainSite} from "../mainSite/MainSite";
 import {Register} from "../register/Register";
+import {Header} from "../header/Header";
 
 function App() {
     return (
-
-        <Routes>
-            <Route path={"/"} element={<MainSite/>}/>
-            <Route path={"/login"} element={<Login/>}/>
-            <Route path={"/register"} element={<Register/>}/>
-        </Routes>
-
+        <>
+            <Header/>
+            <Routes>
+                <Route path={"/"} element={<MainSite/>}/>
+                <Route path={"/login"} element={<Login/>}/>
+                <Route path={"/register"} element={<Register/>}/>
+            </Routes>
+        </>
     );
 }
 
