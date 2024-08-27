4b4a014f1fe5c96fac329c3350683c552c6b74b7
Marian Figula <majofigulam3@gmail.com>
2024-08-27 12:51:38 +0200

    fix: response codes not in json_encode

--- php/api/user/login.php
+++ php/api/user/login.php
@@ -12,7 +12,8 @@ $method = $_SERVER['REQUEST_METHOD'];
 $user = new User($db);
 
 if ($method == "GET"){
-    echo json_encode(["code" => http_response_code(400),
+    http_response_code(405);
+    echo json_encode(["success" => false,
         "message" => "Method not allowed."]);
     exit();
 }
@@ -20,9 +21,9 @@ if ($method == "GET"){
 $data = json_decode(file_get_contents("php://input"));
 
 if (!isset($data->email) || !isset($data->password)){
+    http_response_code(400);
     echo json_encode([
         "success" => false,
-        "code" => http_response_code(400),
         "message" => "Email and password are required."
     ]);
     exit();
@@ -33,25 +34,25 @@ $stmt = $user->getUserByEmail();
 $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
 if (!$row){
+    http_response_code(404);
     echo json_encode([
         "success" => false,
-        "code" => http_response_code(404),
         "message" => "User not found."
     ]);
     exit();
 }
 
 if (!password_verify($data->password, $row['password'])){
+    http_response_code(401);
     echo json_encode([
         "success" => false,
-        "code" => http_response_code(401),
         "message" => "Invalid email or password.",
     ]);
     exit();
 }
 
+http_response_code(200);
 echo json_encode([
     "success" => true,
-    "code" => http_response_code(200),
     "message" => "Login successful.",
 ]);
