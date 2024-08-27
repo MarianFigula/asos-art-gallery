d5667b283e7da10ad1677fa4dc0f89aee2955cbe
Marian Figula <majofigulam3@gmail.com>
2024-08-27 09:48:52 +0200

    feat: created login form

---
+++ src/components/login/Login.js
@@ -0,0 +1,63 @@
+import React, {useState} from "react";
+import {useNavigate} from "react-router-dom";
+import "./Login.css"
+
+export function Login() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [error, setError] = useState("")
+    const navigate = useNavigate()
+
+    async function handleSubmit(event){
+        event.preventDefault()
+
+        try {
+            const response = await fetch("php/api/user/login.php", {
+                    method: "POST",
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({email, password})
+                })
+
+            const data = await response.json()
+            data.success ? navigate("/dasboard") : setError(data.message)
+
+        } catch (error){
+            setError("Login failed. Please try again")
+        }
+
+    }
+
+    return (
+        <div className="login-container">
+            <h2>Login</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Email:</label>
+                    <input
+                        type="email"
+                        value={email}
+                        onChange={(e) =>
+                            setEmail(e.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <label>Password:</label>
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) =>
+                            setPassword(e.target.value)}
+                        required
+                    />
+                </div>
+                {error && <p style={{color: 'red'}}>{error}</p>}
+                <button type="submit">Login</button>
+            </form>
+        </div>
+
+
+    )
+}
