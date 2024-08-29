698d176f4bbfacd6495d40bf467e860a2135afdd
Marian Figula <majofigulam3@gmail.com>
2024-08-29 08:09:03 +0200

    feat: AdminSite with basic table of users, with search bar

--- src/components/adminSite/AdminSite.js
+++ src/components/adminSite/AdminSite.js
@@ -1,82 +1,77 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import DataTable from "react-data-table-component";
+
 export function AdminSite() {
     const columns = [
         {
-            name: 'Title',
-            selector: row => row.name,
+            name: 'Email',
+            selector: row => row.email,
             sortable: true,
         },
         {
-            name: 'Email',
-            selector: row => row.email,
+            name: 'Security Question',
+            selector: row => row.security_question,
             sortable: true
         },
         {
-            name: "Age",
-            selector: row => row.age
+            name: "Security Answer",
+            selector: row => row.security_answer,
+            sortable: true
         }
     ]
 
-    const data = [
-        {
-            id: 1,
-            name: 'Majo',
-            email: "majo@azet.sk",
-            age: 13
-        },
-        {
-            id: 2,
-            name: 'Anna',
-            email: "Anna@gmail.sk",
-            age: 12
-        },
-        {
-            id: 3,
-            name: 'jozo',
-            email: "jozo@email.sk",
-            age: 12
-        },
-        {
-            id: 4,
-            name: 'samo',
-            email: "samo@centrum.sk",
-            age: 12
-        },{
-            id: 3,
-            name: 'jozo',
-            email: "jozo@email.sk",
-            age: 12
-        },
-        {
-            id: 4,
-            name: 'samo',
-            email: "samo@centrum.sk",
-            age: 12
-        },
-    ]
+    const [data, setData] = useState([])
+    const [dependency, setDependency] = useState(false);
 
-    const [records,setRecords] = useState(data)
-    const handleChange = ({ selectedRows }) => {
+    const [records, setRecords] = useState(data)
+
+    useEffect(() => {
+        const serverUrl = process.env.REACT_APP_SERVER_URL
+        const fetchData = async () => {
+            try {
+                const response =
+                    await fetch(`${serverUrl}/api/user/read.php`, {
+                        method: "POST",
+                        headers: {
+                            'Content-Type': 'application/json',
+                        }
+                    })
+
+                const result = await response.json();
+                setData(result.data);    // Store fetched data in `data`
+                setRecords(result.data); // Initialize `records` with fetched data
+            }catch (error) {
+                console.log(error)
+            }
+        }
+        fetchData()
+    }, [dependency])
+
+    // If you want to manually trigger data refresh
+    const refreshData = () => {
+        setDependency(prev => !prev); // Toggle or change dependency to trigger useEffect
+    }
+    const handleChange = ({selectedRows}) => {
         // You can set state or dispatch with something like Redux so we can use the retrieved data
         console.log('Selected Rows: ', selectedRows);
     };
 
     const handleFilter = (event) => {
+        console.log(data)
         const newData = data.filter(row => {
-            return row.name.toLocaleLowerCase()
-                .includes(event.target.value.toLocaleLowerCase())
-            || row.email.toLocaleLowerCase()
-                    .includes(event.target.value.toLocaleLowerCase())
-            || row.age.toString().toLowerCase()
-                    .includes(event.target.value.toString()
-                        .toLowerCase())
-        })
-        setRecords(newData)
-    }
+            return row.email.toLowerCase()
+                    .includes(event.target.value.toLowerCase()) ||
+                row.security_question.toLowerCase()
+                    .includes(event.target.value.toLowerCase()) ||
+                row.security_answer.toLowerCase()
+                    .includes(event.target.value.toLowerCase());
+        });
+        setRecords(newData);
+    };
 
     return (
         <div className=''>
+            <button onClick={refreshData}>RefreshData</button>
             <div className='input'>
                 <input type="text" onChange={handleFilter}/></div>
             <DataTable
