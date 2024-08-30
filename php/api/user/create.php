89c4bdd1b0043f299aeda379f62a50c40a83e800
Marian Figula <majofigulam3@gmail.com>
2024-08-30 17:09:36 +0200

    fix: added username to create.php

--- php/api/user/create.php
+++ php/api/user/create.php
@@ -14,6 +14,7 @@ $user = new User($db);
 $data = json_decode(file_get_contents("php://input"));
 
 if (empty($data->email) || empty($data->password) ||
+    empty($data->username) ||
     empty($data->security_question) || empty($data->security_answer)) {
     echo json_encode(["success" => false, "code" => http_response_code(400) ,
         "message" => "Incomplete data"]);
@@ -21,6 +22,7 @@ if (empty($data->email) || empty($data->password) ||
 }
 
 $user->setEmail($data->email);
+$user->setUsername($data->username);
 $user->setPassword(password_hash($data->password, PASSWORD_BCRYPT));
 $user->setSecurityQuestion($data->security_question);
 $user->setSecurityAnswer($data->security_answer);
