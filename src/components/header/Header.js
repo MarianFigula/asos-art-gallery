cd1e4b1dc51fa56ef311357448b5cd94bc3d9360
Marian Figula <majofigulam3@gmail.com>
2024-08-30 16:29:38 +0200

    feat: created sidebar with functionality

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,21 +1,37 @@
 // src/components/Header.js
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
 import "./Header.css"
 import userIcon from "../../assets/icons/person.svg"
 import listIcon from "../../assets/icons/list.svg"
+import {SideBar} from "../sidebar/SideBar";
 
-export function Header () {
+export function Header() {
+    const [sidebarVisible, setSidebarVisible] = useState(false);
+
+    const toggleSidebar = () => {
+        console.log("open")
+        console.log("before:", sidebarVisible)
+        setSidebarVisible(prevState => !prevState);
+        console.log("after", sidebarVisible)
+    }
+    const closeSidebar = () => {
+        setSidebarVisible(false);
+    }
     return (
-        <header>
-            <nav>
-                <ul>
-                    <li><Link to="/"><h1>FEI Art Gallery</h1></Link></li>
-                    <li><Link to="/login"><img src={userIcon} alt="User Icon"/></Link></li>
-                    <li><Link to={"#"}><img src={listIcon} alt="Menu Icon"/></Link></li>
-                    {/* Add more links as needed */}
-                </ul>
-            </nav>
-        </header>
+        <>
+            <header>
+                <nav>
+                    <ul>
+                        <li><Link to="/"><h1>FEI Art Gallery</h1></Link></li>
+                        <li><Link to="/login"><img src={userIcon} alt="User Icon"/></Link></li>
+                        <li onClick={toggleSidebar}><Link to={"#"}><img src={listIcon} alt="Menu Icon"/></Link></li>
+                        {/* Add more links as needed */}
+                    </ul>
+                </nav>
+            </header>
+            <div className={`grey-zone ${sidebarVisible ? 'visible' : ''}`} onClick={closeSidebar}></div>
+            <SideBar show={sidebarVisible}/>
+        </>
     );
 }
--- src/components/sidebar/SideBar.js
+++ src/components/sidebar/SideBar.js
@@ -1,9 +1,14 @@
 import React from "react";
 import "./SideBar.css"
 
-export function SideBar(){
+export function SideBar({show}){
     return(
         <>
+            <section className={`text-center sidebar ${show ? "show" : ""}`} id="sidebar-id">
+                <h1 className="sidebar-title">
+                    FEI Art Gallery
+                </h1>
+            </section>
         </>
     )
 }
