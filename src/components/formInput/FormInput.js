b408a554b6825a954c092d5a30ff73677354fb36
Marian Figula <majofigulam3@gmail.com>
2024-08-31 09:43:43 +0200

    fix: ternary operator completed

--- src/components/formInput/FormInput.js
+++ src/components/formInput/FormInput.js
@@ -12,25 +12,34 @@ export function FormInput({
                           }) {
 
     return (
-        <div className="mb-0-25">
-            <label className="label">{label}</label>
-            {type === 'select'} ? (
-            <select
-                value={value}
-                onChange={onChange}
-                className="input"
-                required={required}
-                {...props}>
-
-                {options.map((option, index) => (
-                    <option
-                        key={index}
-                        value={index === 0 ? "" : option}>
-                        {option}
-                    </option>
-                ))}
-            </select>
-            )
+        <div>
+            <label className="label mb-0-25">{label}</label>
+            {type === 'select' ? (
+                <select
+                    value={value}
+                    onChange={onChange}
+                    className="input"
+                    required={required}
+                    {...props}
+                >
+                    {options.map((option, index) => (
+                        <option
+                            key={index}
+                            value={index === 0 ? "" : option}>
+                            {option}
+                        </option>
+                    ))}
+                </select>
+            ) : (
+                    <input
+                        type={type}
+                        value={value}
+                        onChange={onChange}
+                        className="input"
+                        required={required}
+                        {...props}
+                    />
+            )}
         </div>
     )
 }
