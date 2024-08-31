091d70f3ca6117674315feb47cf2a4bbf72a1c96
Marian Figula <majofigulam3@gmail.com>
2024-08-31 14:46:47 +0200

    feat: path to be lowercase with `-`

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -16,11 +16,11 @@ function App() {
             <Header/>
             <Routes>
                 <Route path={"/"} element={<MainSite/>}/>
-                <Route path={"/userProfile"} element={<UserProfile/>}/>
+                <Route path={"/user-profile"} element={<UserProfile/>}/>
                 <Route path={"/login"} element={<Login/>}/>
                 <Route path={"/register"} element={<Register/>}/>
                 <Route path={"/admin"} element={<AdminSite/>}/>
-                <Route path={"/adminEditUser"} element={<EditUserAdmin/>}/>
+                <Route path={"/admin-edit-user"} element={<EditUserAdmin/>}/>
             </Routes>
             <Footer />
         </>
