fe3f676132afd1d720f6a3a3c7e0a9149e85424a
Marian Figula <majofigulam3@gmail.com>
2024-08-31 14:44:56 +0200

    feat: stopPropagation() function to prevent closing modal when clicking on ti

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -9,7 +9,7 @@ export function Modal({isOpen, onClose, title, children}){
 
     return (
         <div className="modal-overlay" onClick={onClose}>
-            <div className="modal-content">
+            <div className="modal-content" onClick={(e) => e.stopPropagation()}>
                 <div className="modal-header">
                     <h2>{title}</h2>
                     <button onClick={onClose} className="modal-close-button">
