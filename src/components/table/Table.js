0f9307b90271c05b701e35e7ef0865af3ea713f7
Marian Figula <majofigulam3@gmail.com>
2024-08-31 10:03:11 +0200

    feat: added input id to props

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -4,13 +4,20 @@ import RefreshIcon from "../../assets/icons/arrow-repeat.svg";
 import DataTable from "react-data-table-component";
 
 
-export function Table({columns, records, handleFilter, handleChange, refreshData}) {
+export function Table({
+                          columns,
+                          records,
+                          handleFilter,
+                          handleChange,
+                          refreshData,
+                          searchId
+                      }) {
 
     return (
         <div className='table-wrapper'>
             <div className="search-wrapper mb-1">
-                <label htmlFor="search" className="label">
-                    <input type="search" id="search" className="input border-15" onChange={handleFilter}
+                <label htmlFor={searchId} className="label">
+                    <input type="search" id={searchId} className="input border-15" onChange={handleFilter}
                            placeholder="Search"/>
                     <img src={SearchIcon} alt="Search Icon" className="search-icon"/>
                 </label>
