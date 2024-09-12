accee4db31c34832a8549aa22318c89e37ea42f8
Marian Figula <majofigulam3@gmail.com>
2024-09-12 16:35:40 +0200

    refactor: variables refactoring

--- php/classes/Art.php
+++ php/classes/Art.php
@@ -5,11 +5,11 @@ class Art {
     private $table_name = "art";
     private $id;
     private $user_id;
-    private $img_src;
+    private $img_url;
     private $title;
     private $description;
     private $price;
-    private $art_creation_date;
+    private $upload_date;
 
     public function __construct($db) { $this->conn = $db;}
 
@@ -21,8 +21,8 @@ class Art {
     public function setId($id) { $this->id = $id;}
     public function getUserId() { return $this->user_id;}
     public function setUserId($user_id) {$this->user_id = $user_id;}
-    public function getImgSrc() { return $this->img_src;}
-    public function setImgSrc($img_src) { $this->img_src = $img_src;}
+    public function getImgUrl() { return $this->img_url;}
+    public function setImgUrl($img_url) { $this->img_url = $img_url;}
     public function getTitle() {return $this->title;}
     public function setTitle($title) {$this->title = $title;}
     public function getDescription() {return $this->description;}
@@ -31,15 +31,15 @@ class Art {
     }
     public function getPrice() {return $this->price;}
     public function setPrice($price) {$this->price = $price;}
-    public function getArtCreationDate() {return $this->art_creation_date;}
-    public function setArtCreationDate($art_creation_date) {
-        $this->art_creation_date = $art_creation_date;
+    public function getUploadDate() {return $this->upload_date;}
+    public function setUploadDate($upload_date) {
+        $this->upload_date = $upload_date;
     }
 
 
 
     public function createArt() {
-        $this->art_creation_date = date('Y-m-d H:i:s');
+        $this->upload_date = date('Y-m-d H:i:s');
 
         $query = "INSERT INTO " . $this->table_name . " (user_id, img_src, title, 
         description, price, art_creation_date)
@@ -50,18 +50,18 @@ class Art {
 
         // Sanitize input, TODO: unsanitize, zmenit to tak ze to dat do query
         $this->user_id = htmlspecialchars(strip_tags($this->user_id));
-        $this->img_src = htmlspecialchars(strip_tags($this->img_src));
+        $this->img_url = htmlspecialchars(strip_tags($this->img_url));
         $this->title = htmlspecialchars(strip_tags($this->title));
         $this->description = htmlspecialchars(strip_tags($this->description));
         $this->price = htmlspecialchars(strip_tags($this->price));
-        $this->art_creation_date = htmlspecialchars(strip_tags($this->art_creation_date));
+        $this->upload_date = htmlspecialchars(strip_tags($this->upload_date));
 
         $stmt->bindParam(":user_id", $this->user_id);
-        $stmt->bindParam(":img_src", $this->img_src);
+        $stmt->bindParam(":img_src", $this->img_url);
         $stmt->bindParam(":title", $this->title);
         $stmt->bindParam(":description", $this->description);
         $stmt->bindParam(":price", $this->price);
-        $stmt->bindParam(":art_creation_date", $this->art_creation_date);
+        $stmt->bindParam(":art_creation_date", $this->upload_date);
 
         return $stmt->execute();
 
