0b555c43650f80b072bfeae390c39a917ef838f9
Marian Figula <majofigulam3@gmail.com>
2024-09-13 09:09:31 +0200

    feat: using updateHandler function

--- php/api/user/update.php
+++ php/api/user/update.php
@@ -58,26 +58,4 @@ $user->setUsername($username);
 $user->setEmail($email);
 
 $stmt = $user->updateUserById();
-if (!$stmt){
-    http_response_code(400);
-    echo json_encode([
-        "success" => false,
-        "message" =>  "Update failed"]);
-    exit();
-}
-
-if ($stmt->rowCount() === 0) {
-    http_response_code(400);
-    echo json_encode([
-        "success" => false,
-        "message" => "No rows were updated. 
-        Either the user does not exist or no changes were made."
-    ]);
-    exit();
-}
-
-http_response_code(200);
-echo json_encode([
-    "success" => true,
-    "message" => "User successfully updated."]);
-exit();
+updateHandler($stmt);
