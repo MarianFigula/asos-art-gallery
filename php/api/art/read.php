7bc241622fa625886dfc7af16e6b378e36faf666
Marian Figula <majofigulam3@gmail.com>
2024-09-13 15:10:52 +0200

    feat: getting user by email and then arts by user_id

--- php/api/art/read.php
+++ php/api/art/read.php
@@ -3,6 +3,7 @@
 header("Content-Type: application/json");
 include_once '../../config/Database.php';
 include_once '../../classes/Art.php';
+include_once '../../classes/User.php';
 include_once "../../config/cors.php";
 
 $database = new Database();
@@ -26,6 +27,37 @@ if ($method == "GET"){
 if (isset($data->user_id)){
     $art->setUserId($data->user_id);
     $stmt = $art->getArtsByUserId();
+    $arts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    http_response_code(200);
+    echo json_encode([
+        "success" => true,
+        "data" => $arts]);
+    exit();
+}
+
+if (!empty($data->user_email)) {
+    $user = new User($db);
+    $user->setEmail($data->user_email);
+
+    $stmt = $user->getUserByEmail();
+
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    if (!$row) {
+        http_response_code(404);
+        echo json_encode([
+            "success" => false,
+            "message" => "User not found."]);
+        exit();
+    }
+
+    $user_id = $row["id"];
+
+    $user->setId($user_id);
+    $art->setUserId($user_id);
+
+    $stmt = $art->getArtsByUserId();
+
     $arts = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
     http_response_code(200);
