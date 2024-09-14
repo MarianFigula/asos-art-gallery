1600aad988ef37b1deef0be84d8ddb57a8633f98
Marian Figula <majofigulam3@gmail.com>
2024-09-14 15:18:10 +0200

    feat: wrapper for search and sorting buttons

--- src/components/mainSite/MainSite.js
+++ src/components/mainSite/MainSite.js
@@ -1,11 +1,29 @@
-
-
+import {SearchBar} from "../searchBar/SearchBar";
+import "./MainSite.css"
 
 export function MainSite() {
+    const handleFilter = (event) => {
+
+    }
+
     return (
         <>
-            <h1>Welcome to art gallery</h1>
+            <h1 className="text-center">Discover new Arts</h1>
+            <section className="main-wrapper">
+                <SearchBar
+                searchId="main-searchbar"
+                handleFilter={handleFilter}/>
 
+                <div className="button-wrapper">
+                    <button>New</button>
+                    <button>Price <i className="bi bi-arrow-up"></i></button>
+                    <button>Price <i className="bi bi-arrow-down"></i></button>
+                    <button>Rating <i className="bi bi-arrow-up"></i></button>
+                    <button>Rating <i className="bi bi-arrow-down"></i></button>
+                </div>
+
+
+            </section>
         </>
 
     )
