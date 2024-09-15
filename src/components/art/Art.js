4a14508cc1b1536aff1020ce25b6625ed3bd62c6
Marian Figula <majofigulam3@gmail.com>
2024-09-15 10:45:23 +0200

    feat: Art component

---
+++ src/components/art/Art.js
@@ -0,0 +1,32 @@
+import {ImageComponent} from "../artImage/artImage";
+
+
+export function Art({art, handleClick}) {
+    return (
+        <div className="art-wrapper">
+            <div className="img-wrapper">
+                <ImageComponent imgUrl={art.img_url}/>
+
+            </div>
+            <div className="img-info">
+                <div className="space-between-for-two-components">
+                    <h3>{art.title}</h3>
+                    <p>{art.username}</p>
+                </div>
+                <p className="art-description">
+                    {art.description}
+                </p>
+            </div>
+            <div className="space-between-for-two-components">
+                <p className="price">{art.price}€</p>
+                <button
+                    className="button-add-to-cart"
+                    onClick={handleClick}
+                >
+                    <i className="bi bi-cart3"></i>
+                    Add to cart
+                </button>
+            </div>
+        </div>
+    )
+}
