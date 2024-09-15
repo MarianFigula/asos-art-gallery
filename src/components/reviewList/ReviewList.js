21037696a60c1240e848599944d34911e0293b26
Marian Figula <majofigulam3@gmail.com>
2024-09-15 10:57:10 +0200

    feat: ReviewList

---
+++ src/components/reviewList/ReviewList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {ReviewItem} from "../reviewItem/ReviewItem";
+
+
+export function ReviewList({ reviews }) {
+    return (
+        <div className="review-wrapper">
+            <div className="space-between-for-two-components">
+                <h3>Reviews</h3>
+                <i className="bi bi-plus-circle"></i>
+            </div>
+            {reviews.map((review, index) => (
+                <ReviewItem
+                    key={index}
+                    username={review.username}
+                    date={review.date}
+                    reviewText={review.reviewText}
+                    stars={review.stars}
+                />
+            ))}
+        </div>
+    );
+}
