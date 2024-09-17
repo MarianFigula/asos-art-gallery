93ed77deb2bcfdd1fec98dca811b2521f008bf74
Marian Figula <majofigulam3@gmail.com>
2024-09-17 08:43:42 +0200

    feat: added onClick to open modal

--- src/components/reviewList/ReviewList.js
+++ src/components/reviewList/ReviewList.js
@@ -2,12 +2,12 @@ import React from 'react';
 import {ReviewItem} from "../reviewItem/ReviewItem";
 
 
-export function ReviewList({ reviews }) {
+export function ReviewList({ reviews, openReviewModal }) {
     return (
         <div className="review-wrapper">
             <div className="space-between-for-two-components">
                 <h3>Reviews</h3>
-                <i className="bi bi-plus-circle"></i>
+                <i className="bi bi-plus-circle" onClick={openReviewModal}></i>
             </div>
             {reviews.length === 0 ? <div className="mt-2 text-center">None</div> :
                 reviews.map((review, index) => (
