81d369621085e5f1453e9fef0f88d231b1942bbe
Marian Figula <majofigulam3@gmail.com>
2024-10-31 08:54:34 +0100

    feat: added headers and content type just to be sure

--- php/config/cors.php
+++ php/config/cors.php
@@ -1,8 +1,9 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Max-Age: 3600"); // Cache the preflight response for 1 hour
+header("Content-Type: application/json");
 
 // Handle preflight OPTIONS request
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
