93e0edb477208f81b9d87a435b5e436aae905ce5
Marian Figula <majofigulam3@gmail.com>
2024-10-31 08:54:40 +0100

    feat: added headers and content type just to be sure

--- php/config/Database.php
+++ php/config/Database.php
@@ -6,11 +6,20 @@ error_reporting(E_ALL);
 error_reporting(E_WARNING);
 
 class Database {
-    private $hostname = 'localhost';
+    // DOCKER
+    private $hostname = 'mysql';
     private $dbname = 'pkrim-art-gallery';
     private $username = 'root';
-    private $password = '';
+    private $password = 'root';
     public $conn;
+
+    // LOCAL
+
+//    private $hostname = 'localhost';
+//    private $dbname = 'pkrim-art-gallery';
+//    private $username = 'root';
+//    private $password = '';
+//    public $conn;
 
     public function getConnection() {
         $this->conn = null;
