faf2e22fbd1139cbe2f0f91c61d109f3b51e4c4b
Marian Figula <majofigulam3@gmail.com>
2024-11-08 17:27:52 +0100

    feat: added onclick function for further displaying modal

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -1,24 +1,23 @@
 import {ArtImage} from "../artImage/artImage";
-import Camera from "../../assets/user-pictures/camera.png"
 
 
-export default function CartItem({artTitle,imgUrl, authorName, price}){
+export default function CartItem({artTitle,imgUrl, authorName, price, handleClick}){
 
 
     return (
         <>
             <div className="cart-item mb-3">
                 <div className="cart-art-info">
-                    <div className="cart-image">
-                        <ArtImage imgUrl={Camera}/>
+                    <div className="cart-image" onClick={handleClick}>
+                        <ArtImage imgUrl={imgUrl}/>
                     </div>
                     <div>
                         <h2>{artTitle}</h2>
                         <p>By {authorName}</p>
                     </div>
                 </div>
-                <i className="bi bi-trash-fill"></i>
                 <h3 className="price">{price} €</h3>
+                <i className="bi bi-trash-fill"></i>
             </div>
         </>
     )
