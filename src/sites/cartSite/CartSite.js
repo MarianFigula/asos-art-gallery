6c47715b504c72487ac4d940f45897c14029ff4e
Marian Figula <majofigulam3@gmail.com>
2024-11-08 18:02:40 +0100

    feat: added test variable to check style if cart is empty or not

--- src/sites/cartSite/CartSite.js
+++ src/sites/cartSite/CartSite.js
@@ -3,15 +3,17 @@ import CartItem from "../../components/CartItem/CartItem";
 
 import Camera from "../../assets/user-pictures/camera.png"
 import Camera2 from "../../assets/user-pictures/camera-2.png"
+
 export default function CartSite() {
 
+    const test = 1;
     return (
-        <>
-            <div className="cart-wrapper">
-                <div className="cart-header">
-                    <h1>Shopping Cart</h1>
-                    <p className="mb-4">Your Items</p>
-                </div>
+        <div className="cart-wrapper">
+            <div className="cart-header">
+                <h1>Shopping Cart</h1>
+                <p className="mb-3">Your Items</p>
+            </div>
+            {test === 1 ?
                 <section className="cart-items">
                     <CartItem
                         artTitle={"arttitle"}
@@ -31,12 +33,13 @@ export default function CartSite() {
                         authorName={"authorname"}
                         price={"100"}
                     />
-
-
                 </section>
+                :
+                <h1 className="text-center">
+                    Your shopping cart is empty
+                </h1>
+            }
+        </div>
 
-            </div>
-
-        </>
     )
 }
