27da5afc0631656f079d0a0ac6800ca161e45534
Marian Figula <majofigulam3@gmail.com>
2024-11-26 20:38:47 +0100

    feat: adding modal into main site to preview art

--- src/components/art/Art.js
+++ src/components/art/Art.js
@@ -1,32 +1,56 @@
 import {ArtImage} from "../artImage/artImage";
+import {Modal} from "../modal/Modal";
+import {useState} from "react";
 
 
-export function Art({art, handleClick}) {
+export function Art({art, handleClick, openArtImageModalClick}) {
+
+    const [isArtImageModalOpen, setIsArtImageModalOpen] = useState(false)
+
+    const openImageModal = () => {
+        setIsArtImageModalOpen(true)
+    }
+
     return (
-        <div className="art-wrapper">
-            <div className="img-wrapper">
-                <ArtImage imgUrl={art.img_url}/>
-
-            </div>
-            <div className="img-info">
-                <div className="space-between-for-two-components">
-                    <h3>{art.title}</h3>
-                    <p>{art.username}</p>
+        <>
+            <Modal
+                isOpen={isArtImageModalOpen}
+                title={art.title}
+                onClose={() => setIsArtImageModalOpen(false)}>
+                <div className="cart-modal-img">
+                    <ArtImage imgUrl={art.img_url}></ArtImage>
                 </div>
-                <p className="art-description">
-                    {art.description}
-                </p>
+                <div className="space-between-for-two-components">
+                    <p>{art.username}</p>
+                    <p className="price">{art.price}€</p>
+                </div>
+            </Modal>
+
+            <div className="art-wrapper">
+                <div className="img-wrapper" onClick={() => setIsArtImageModalOpen(true)}>
+                    <ArtImage imgUrl={art.img_url}/>
+
+                </div>
+                <div className="img-info">
+                    <div className="space-between-for-two-components">
+                        <h3>{art.title}</h3>
+                        <p>{art.username}</p>
+                    </div>
+                    <p className="art-description">
+                        {art.description}
+                    </p>
+                </div>
+                <div className="space-between-for-two-components">
+                    <p className="price">{art.price}€</p>
+                    <button
+                        className="button-add-to-cart"
+                        onClick={handleClick}
+                    >
+                        <i className="bi bi-cart3"></i>
+                        Add to cart
+                    </button>
+                </div>
             </div>
-            <div className="space-between-for-two-components">
-                <p className="price">{art.price}€</p>
-                <button
-                    className="button-add-to-cart"
-                    onClick={handleClick}
-                >
-                    <i className="bi bi-cart3"></i>
-                    Add to cart
-                </button>
-            </div>
-        </div>
+        </>
     )
 }
