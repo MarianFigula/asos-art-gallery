32a5008bce88efcf1de875f64908ee28529010c8
tomasjencik <xjencikt@stuba.sk>
2024-11-29 19:16:16 +0100

    test1: cart button in header switches to CartSite

---
+++ src/components/header/Header.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { MemoryRouter, Routes, Route } from 'react-router-dom';
+import { Header } from './Header';
+import CartSite from '../../sites/cartSite/CartSite.js';
+import '@testing-library/jest-dom';
+
+test('navigates to CartSite when bi bi-cart is clicked', () => {
+    render(
+        <MemoryRouter initialEntries={['/']}>
+            <Routes>
+                <Route path="/" element={<Header />} />
+                <Route path="/cart" element={<CartSite />} />
+            </Routes>
+        </MemoryRouter>
+    );
+
+    const cartLink = screen.getByRole('link', { name: /cart/i });
+
+
+    fireEvent.click(cartLink);
+
+    expect(screen.getByText(/Shopping Cart/i)).toBeInTheDocument();
+});
+
