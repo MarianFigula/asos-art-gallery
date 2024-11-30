b78489a17b2214dd8f047976a628935a8053d123
Marian Figula <majofigulam3@gmail.com>
2024-11-30 16:45:35 +0100

    feat: updated cors for dele method

--- php/config/cors.php
+++ php/config/cors.php
@@ -1,7 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type");
-header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Methods: GET, POST, DELETE, PUT, OPTIONS");
 header("Access-Control-Max-Age: 3600"); // Cache the preflight response for 1 hour
 
 // Handle preflight OPTIONS request
