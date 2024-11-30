0cafdbbff91f349667b0b74eb4ac12c0f8c3d4a9
tomasjencik <xjencikt@stuba.sk>
2024-11-30 01:07:37 +0100

    test7: test to call handleFilter on input change

--- src/components/searchBar/SearchBar.test.js
+++ src/components/searchBar/SearchBar.test.js
@@ -14,3 +14,16 @@ test('test that checks if searchBar renders  with custom placeholder', () => {
     const input = getByPlaceholderText(/test placeholder/i);
     expect(input).toBeInTheDocument();
 });
+
+test('test to call handleFilter on input change', () => {
+    const handleFilter = jest.fn();
+    const { getByRole } = render(<SearchBar searchId="search" handleFilter={handleFilter} />);
+
+    const input = getByRole('searchbox');
+
+    fireEvent.change(input, { target: { value: 'test' } });
+
+    expect(handleFilter).toHaveBeenCalledTimes(1);
+    expect(handleFilter).toHaveBeenCalledWith(expect.any(Object));
+});
+
