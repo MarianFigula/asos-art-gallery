7483bd4b7b29c63c75278816a734c634606e6ad1
Marian Figula <majofigulam3@gmail.com>
2024-12-02 23:05:56 +0100

    fix: removed useeffect

--- src/components/auth/AuthContext.js
+++ src/components/auth/AuthContext.js
@@ -9,13 +9,6 @@ export const AuthProvider = ({ children }) => {
     const [token, setToken] = useState(localStorage.getItem('jwtToken') || null);
     const navigate = useNavigate();
 
-    // Check if token exists or is invalid (you can extend this logic)
-    useEffect(() => {
-        if (!token) {
-            navigate('/login');  // Redirect to login if no token
-        }
-    }, [token, navigate]);
-
     const login = (newToken) => {
         localStorage.setItem('jwtToken', newToken);
         setToken(newToken);
