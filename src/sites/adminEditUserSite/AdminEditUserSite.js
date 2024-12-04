62c774c0e2f676f8e3295902d2ec95f4147e5dcc
Pavol Polednák <pavolpolednak@protonmail.com>
2024-12-04 18:36:55 +0100

    fix: adminedituser scrolling no longer using previous scroll state

--- src/sites/adminEditUserSite/AdminEditUserSite.js
+++ src/sites/adminEditUserSite/AdminEditUserSite.js
@@ -111,6 +111,7 @@ export function AdminEditUserSite() {
 
     useEffect(() => {
         if (id) {
+            window.scrollTo(0, 0);
             fetchUserData();
             fetchArtData();
             fetchReviewData();
